438fd6ee18a8366c46b74e82fb979a7b31b0be16
David Tram <david.tram@glidewelldental.com>
2020-02-09 17:11:19 -0800

    Finish signin and signup

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-1",
+      BUCKET: "construction.image.app"
+    },
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "VirtualAssistant"
+    },
+    cognito: {
+      REGION: "us-east-1",
+      USER_POOL_ID: "us-east-1_GEZzpSYbJ",
+      APP_CLIENT_ID: "7g0i5qgmj12kcietuhsa5j8h2l",
+      IDENTITY_POOL_ID: "us-east-1:2f80391d-f0b5-4970-854f-11ab47e5414a"
+    }
+  };
